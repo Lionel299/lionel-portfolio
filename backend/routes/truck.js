fdf567fb5b781700538f52968f532ffc6006fa47
Nkomha Lionel <nkomhalionel@gmail.com>
2025-06-27 10:13:32 +0100

    commiting

--- backend/routes/truck.js
+++ backend/routes/truck.js
@@ -4,10 +4,10 @@ const truckController = require('../controllers/truckController');
 
 // CRUD routes
 
-router.post('/trucks', truckController.createTruck);
-router.get('/trucks', truckController.getTrucks);
-router.get('/trucks/:id', truckController.getTruckById);
-router.put('/trucks/:id', truckController.updateTruck);
-router.delete('/trucks/:id', truckController.deleteTruck);
+router.post('/', truckController.createTruck);
+router.get('/', truckController.getTrucks);
+router.get('/:id', truckController.getTruckById);
+router.put('/:id', truckController.updateTruck);
+router.delete('/:id', truckController.deleteTruck);
 
 module.exports = router;
